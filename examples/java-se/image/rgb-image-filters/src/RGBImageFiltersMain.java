c72fe4f830ac165b07c0f3927001676600b16c66
andbin <andbin@users.noreply.github.com>
2016-03-05 15:23:31 +0100

    Improve main class code in rgb-image-filters example

--- examples/java-se/image/rgb-image-filters/src/RGBImageFiltersMain.java
+++ examples/java-se/image/rgb-image-filters/src/RGBImageFiltersMain.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Andrea Binello ("andbin")
+ * Copyright (C) 2015-2016 Andrea Binello ("andbin")
  *
  * This file is part of the "Java Examples" project and is licensed under the
  * MIT License. See one of the license files included in the root of the project
@@ -17,44 +17,50 @@ import javax.swing.SwingUtilities;
 
 /*
  * This is the "main" class of this Java Example.
+ *
+ * The SwingUtilities.invokeLater causes the run() to be executed asynchronously
+ * in the context of the EDT (Event Dispatch Thread).
+ *
+ * It is generally always a good practice to start all the GUI creation on the
+ * EDT and *not* on the "main" thread.
  */
 public class RGBImageFiltersMain {
-	private Image originalImage;
-	private Image negativeImage;
-	private Image swingGrayImage;
-	private Image avgDesaturationImage;
-	private Image hueShiftImage;
-	private Image redPassImage;
-	private Image greenPassImage;
-	private Image bluePassImage;
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				RGBImageFiltersMain main = new RGBImageFiltersMain();
+				main.createAndShowGUI();
+			}
+		});
+	}
 
-	private void setupImages() {
-		originalImage = loadImage("images/colored-pencils.png");
+	private void createAndShowGUI() {
+		// Original image loaded from image resource.
+		Image originalImage = loadImage("images/colored-pencils.png");
 
 		// FILTER 1: a "negative" filter.
-		negativeImage = filterImage(originalImage, new NegativeImageFilter());
+		Image negativeImage = filterImage(originalImage, new NegativeImageFilter());
 
 		// FILTER 2: a "gray" filter using the standard javax.swing.GrayFilter class.
 		// See javadoc documentation of GrayFilter constructor.
-		swingGrayImage = filterImage(originalImage, new GrayFilter(true, 40));
+		Image swingGrayImage = filterImage(originalImage, new GrayFilter(true, 40));
 
 		// FILTER 3: an "average desaturation" filter.
-		avgDesaturationImage = filterImage(originalImage, new AverageDesaturationImageFilter());
+		Image avgDesaturationImage = filterImage(originalImage, new AverageDesaturationImageFilter());
 
 		// FILTER 4: a "hue shift" filter adding +1/6.
-		hueShiftImage = filterImage(originalImage, new HueShiftImageFilter(1.0f / 6));
+		Image hueShiftImage = filterImage(originalImage, new HueShiftImageFilter(1.0f / 6));
 
 		// FILTER 5: a "red pass" filter.
-		redPassImage = filterImage(originalImage, ColorMaskImageFilter.getRedPass());
+		Image redPassImage = filterImage(originalImage, ColorMaskImageFilter.getRedPass());
 
 		// FILTER 6: a "green pass" filter.
-		greenPassImage = filterImage(originalImage, ColorMaskImageFilter.getGreenPass());
+		Image greenPassImage = filterImage(originalImage, ColorMaskImageFilter.getGreenPass());
 
 		// FILTER 7: a "blue pass" filter.
-		bluePassImage = filterImage(originalImage, ColorMaskImageFilter.getBluePass());
-	}
+		Image bluePassImage = filterImage(originalImage, ColorMaskImageFilter.getBluePass());
 
-	private void showDemoGui() {
+		// Creates and shows GUI.
 		RGBImageFiltersFrame frame = new RGBImageFiltersFrame(0, 4, 16, 16);
 		frame.addImageBox(originalImage, "ORIGINAL");
 		frame.addImageBox(negativeImage, "NEGATIVE");
@@ -71,33 +77,20 @@ public class RGBImageFiltersMain {
 	}
 
 	private Image loadImage(String resourceName) {
-		// The image file is searched using the "resource" concept. See the
+		// Finds the image file using the "resource" concept. See the
 		// documentation of getResource/getResourceAsStream in java.lang.Class.
 		URL imageUrl = getClass().getResource(resourceName);
 		return Toolkit.getDefaultToolkit().getImage(imageUrl);
 	}
 
+	/*
+	 * This method applies the filter (ImageFilter) to an Image creating and
+	 * returning a *new* Image.
+	 */
 	private static Image filterImage(Image image, ImageFilter imageFilter) {
 		ImageProducer producer = image.getSource();
 		FilteredImageSource source = new FilteredImageSource(producer, imageFilter);
 		Image filteredImage = Toolkit.getDefaultToolkit().createImage(source);
 		return filteredImage;
-	}
-
-	/*
-	 * The SwingUtilities.invokeLater causes the run() to be executed asynchronously
-	 * in the context of the EDT (Event Dispatch Thread).
-	 *
-	 * It is generally always a good practice to start all the GUI creation on the
-	 * EDT and *not* on the "main" thread.
-	 */
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				RGBImageFiltersMain main = new RGBImageFiltersMain();
-				main.setupImages();
-				main.showDemoGui();
-			}
-		});
 	}
 }
